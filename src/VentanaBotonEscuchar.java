fa2f89dae278ba3028eb6870adffa974772e2209
Said Mariano <Said Mariano@Said>
2020-10-15 17:31:42 -0500

    1_3_2

---
+++ src/VentanaBotonEscuchar.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Date;
+
+/**
+ *
+ * @author Said Mariano
+ */
+public class VentanaBotonEscuchar extends JFrame implements ActionListener{
+    JPanel panelBoton;
+    JLabel etiqueta;
+    JButton boton;
+    
+    public VentanaBotonEscuchar (){
+    etiqueta = new JLabel();
+    panelBoton = new JPanel();
+    boton = new JButton();
+    panelBoton.add(boton);
+    this.getContentPane().setLayout(new BorderLayout());
+    this.getContentPane().add(etiqueta, "North");
+    this.getContentPane().add(panelBoton, "South");
+    boton.addActionListener(this);
+    this.setSize(300,100);
+    this.setTitle("Ejemplo de un boton con Evento 2");
+    this.show();
+}
+    
+    @Override
+    public void actionPerformed(ActionEvent e){
+        etiqueta.setText("Boton pulsado: " + new Date());
+    }
+    
+    public static void main(String[] args){
+        new VentanaBotonEscuchar();
+    }
+            
+}
